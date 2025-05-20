acebc83150a8e115ef8c7caae17fcc5c0e3f4f6d
Muhammad Danish <danishkanirya9999@gmail.com>
2025-05-20 08:50:08 +0500

    restoring vexora labs project in window

---
+++ Vexora Labs/Data/Migrations/20250519084435_RestoreToWindow.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vexora_Labs.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestoreToWindow : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
